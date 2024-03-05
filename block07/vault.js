1e2350c6a7e11216434d3bb6164de64b4c747745
hchj03 <hermann.hopson@gmail.com>
2024-03-05 00:12:37 -0600

    new file:   vault.html 	new file:   vault.js

---
+++ block07/vault.js
@@ -0,0 +1,19 @@
+/* psuedocode
+1.you've been selected alert
+2. codes 1,2 and 3
+3. failure or sucess window
+*/
+
+// selection screen
+
+const introMsg = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// first code
+let code1 = 6*6; //36
+
+//second code
+let code2 = 10 + code1; //46
+
+//third code 
+let code3 = code2/2; //23
+
